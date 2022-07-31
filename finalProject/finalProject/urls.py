e0371d9311eab007a899f6beb8c3b489635a53ae
lenovo <anna.wieczerzak@yahoo.com>
2022-07-31 10:27:27 +0200

    Anna Wieczerzak base.html still not found

--- finalProject/finalProject/urls.py
+++ finalProject/finalProject/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.urls import path
+from django.views.generic import TemplateView
+
+from main.views import HomeView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('base/', HomeView.as_view()),
 ]
