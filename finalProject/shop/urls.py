9afa5802dc3de9c7b95cd41a4a9ed890353e016e
piegusss <podpirkopaulina@gmail.com>
2022-08-06 21:27:34 +0200

    fixed catagory_list view, product_detail - views work, still problem with template

--- finalProject/shop/urls.py
+++ finalProject/shop/urls.py
@@ -1,22 +1,13 @@
-from django.contrib import admin
-from django.urls import path, include
 from shop.views import *
-
-
-
-
 from django.urls import path
-
-
 
 
 app_name = 'shop'
 urlpatterns = [
-                path('categories', CategoryListView.as_view(), name='category_list'),
+                # path('categories', CategoryListView.as_view(), name='category_list'),
                 path('category/<int:pk>', CategoryDetailView.as_view(), name='category_detail'),
-                path('product/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
-
-
-
+                # path('product/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+                path("categories/", get_categories, name="category_list"),
+                path('product/<int:id>', get_product_detail, name='product_detail'),
 
 ]
--- finalProject/shop/views.py
+++ finalProject/shop/views.py
@@ -2,24 +2,31 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.views import View
 
-
 from .models import Product, Category
 
 
-class ProductDetailView(View):
-    model = Product
-    context_object_name = 'product'
-    template_name = 'shop/product_detail.html'
-    queryset = Product.objects.all()
+# class ProductDetailView(View):
+#     model = Product
+#     context_object_name = 'product'
+#     template_name = 'shop/product_detail.html'
+#     queryset = Product.objects.all()
+#
+#     def get_object(self):
+#         pk = self.kwargs.get('pk')
+#         return get_object_or_404(Product, pk=id)
 
-    def get_object(self):
-        pk = self.kwargs.get('pk')
-        return get_object_or_404(Product, pk=id)
+def get_product_detail(request, id):
+    product = Product.objects.get(id=id)
+    context = {'product': product}
 
-class CategoryListView(ListView):
-    model = Category
-    context_object_name = 'categories'
-    template_name = 'shop/category_list.html'
+    return render(request, "shop/product_detail.html",context)
+
+
+def get_categories(request):
+    categories = Category.objects.all()
+    ctx = {"categories": categories}
+
+    return render(request, "shop/category_list.html", context=ctx)
 
 
 class CategoryDetailView(DetailView):
@@ -29,16 +36,5 @@ class CategoryDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # context['products'] = Product.objects.all()
-        context['products'] = Product.objects.filter(category = self.get_object())
+        context['products'] = Product.objects.filter(category=self.get_object())
         return context
-
-
-
-
-
-
-
-
-
-
