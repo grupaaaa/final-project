a19161391647ac28f8a2c045835918f542033e05
Your Name <alak229@gmail.com>
2022-08-21 15:33:01 +0200

    add password change done-update

--- finalProject/main/views.py
+++ finalProject/main/views.py
@@ -9,6 +9,6 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
-        if self.request.user.is_authenticated:
-            context['order_id'] = self.request.user.order_set.filter(status=OrderStatusChoice.INITIAL).first().id
+        # if self.request.user.is_authenticated:
+        #     context['order_id'] = self.request.user.order_set.filter(status=OrderStatusChoice.INITIAL).first().id
         return context
