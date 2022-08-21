d11d511d67ceabeeb51e07a7806691bc57951051
Your Name <alak229@gmail.com>
2022-08-21 11:16:12 +0200

    account password change done!

--- finalProject/accounts/urls.py
+++ finalProject/accounts/urls.py
@@ -1,13 +1,22 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic.base import TemplateView
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
+from django.urls import path, include, reverse_lazy
 from accounts.views import register
-
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', include("django.contrib.auth.urls")),
-    # path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path("", include("django.contrib.auth.urls")),
     path('register/', register, name='register'),
+    path('change_password/',
+         PasswordChangeView.as_view(
+             template_name="registration/password_change_form.html",
+             success_url=reverse_lazy("accounts:password_change_done")
+         ),
+         name='password_change'),
+
+    path('password_change/done/', PasswordChangeDoneView.as_view(
+        template_name='registration/password_change_done.html'
+        ),
+        name='password_change_done')
+
 ]
